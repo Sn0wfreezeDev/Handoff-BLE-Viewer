9fc2e6c57a491331f7c8e12c0a75416ab8387d69
Alexander Heinrich <alexander@alex-heinrich.dev>
2019-09-24 18:01:56 +0200

    Initial Commit

---
+++ BLE-Viewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BLE-Viewer
+//
+//  Created by Alexander Heinrich on 24.09.19.
+//  Copyright © 2019 Alexander Heinrich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BLE-ViewerTests/BLE_ViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  BLE_ViewerTests.swift
+//  BLE-ViewerTests
+//
+//  Created by Alexander Heinrich on 24.09.19.
+//  Copyright © 2019 Alexander Heinrich. All rights reserved.
+//
+
+import XCTest
+@testable import BLE_Viewer
+
+class BLE_ViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
